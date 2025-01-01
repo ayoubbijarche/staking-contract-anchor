fb34c5b3aa65bd34d6e82e06a419b715bfd4a3da
ayoubbijarche <ayoubbijarche6@gmail.com>
2025-01-01 14:35:33 +0100

    first commit

---
+++ programs/stakingcontract/src/lib.rs
@@ -0,0 +1,241 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, MintTo, Transfer};
+use anchor_spl::token_interface::{Mint , TokenAccount , TokenInterface};
+
+
+
+declare_id!("8xHxL2EX8StDf1VQbNQRt4D7UvNPtBfn8nnPND7ZvVzd");
+
+#[program]
+pub mod stakingcontract {
+
+    use super::*;
+
+    pub fn initialize(ctx : Context<Initialize> , start_slot : u64 , end_slot : u64) -> Result<()> {
+        let poolinfo = &mut ctx.accounts.pool_info;
+        poolinfo.admin = ctx.accounts.admin.key();
+        poolinfo.start_slot = start_slot;
+        poolinfo.end_slot = end_slot;
+        poolinfo.token = ctx.accounts.staking_token.key();
+        Ok(())
+    }
+
+    pub fn stake(ctx : Context<Stake> , amount : u64) -> Result<()>{
+        let userinfo = &mut ctx.accounts.user_info;
+        let clock = Clock::get()?;
+        
+        if userinfo.amount > 0 {
+            let reward = (clock.slot - userinfo.deposit_slot) - userinfo.debt_reward;
+            let cpi_accounts = MintTo{
+                mint: ctx.accounts.staking_token.to_account_info(),
+                to : ctx.accounts.user_staking_wallet.to_account_info(),
+                authority : ctx.accounts.admin_staking_wallet.to_account_info(),
+            };
+
+            let cpi_program = ctx.accounts.token_program.to_account_info();
+            let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+            token::mint_to(cpi_ctx, reward)?;
+        }
+
+        let cpi_accounts = Transfer{
+            from : ctx.accounts.user_staking_wallet.to_account_info(),
+            to : ctx.accounts.admin_staking_wallet.to_account_info(),
+            authority : ctx.accounts.user.to_account_info()
+        };
+        
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
+        userinfo.amount += amount;
+        userinfo.deposit_slot = clock.slot;
+        userinfo.debt_reward = 0;
+        Ok(())
+    }
+
+    pub fn unstake(ctx : Context<Unstake>) -> Result<()>{
+        let userinfo = &mut ctx.accounts.user_info;
+        let clock = Clock::get()?;
+        let reward = (clock.slot - userinfo.deposit_slot) - userinfo.debt_reward;
+
+        let cpi_accounts = MintTo{
+            mint : ctx.accounts.staking_token.to_account_info(),
+            to : ctx.accounts.user_staking_wallet.to_account_info(),
+            authority : ctx.accounts.admin_staking_wallet.to_account_info()
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::mint_to(cpi_ctx, reward)?;
+        
+        let cpi_accounts = Transfer{
+            from : ctx.accounts.admin_staking_wallet.to_account_info(),
+            to : ctx.accounts.user_staking_wallet.to_account_info(),
+            authority : ctx.accounts.admin.to_account_info(),
+        };
+        
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program , cpi_accounts);
+        token::transfer(cpi_ctx, userinfo.amount)?;
+
+        userinfo.amount = 0;
+        userinfo.deposit_slot = 0;
+        userinfo.debt_reward = 0;
+
+        Ok(())
+    }
+
+    pub fn claim_reward(ctx: Context<Claimreward>) -> Result<()> {
+        let user_info = &mut ctx.accounts.user_info;
+        let clock = Clock::get()?;
+        let reward = (clock.slot - user_info.deposit_slot) - user_info.debt_reward;
+        let cpi_accounts = MintTo{
+            mint : ctx.accounts.staking_token.to_account_info(),
+            to : ctx.accounts.user_staking_wallet.to_account_info(),
+            authority : ctx.accounts.admin.to_account_info()
+        };
+        
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::mint_to(cpi_ctx, reward)?;
+
+        user_info.debt_reward += reward;
+
+        Ok(())
+    }
+
+
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info>{
+    #[account(mut)]
+    pub admin : Signer<'info>,
+    #[account(
+      init,
+      payer = admin,
+      space = 8 + PoolInfo::LEN
+    )]
+    pub pool_info : Account<'info , PoolInfo>,
+    #[account(mut)]
+    pub staking_token : InterfaceAccount<'info , Mint>,
+    pub system_program : Program<'info , System>,
+}
+
+#[derive(Accounts)]
+pub struct Stake<'info>{
+    #[account(mut)]
+    pub user : Signer<'info>,
+    #[account(mut)]
+    pub admin : AccountInfo<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = 8 + UserInfo::LEN
+    )]
+    pub user_info : Account<'info , UserInfo>,
+
+    #[account(mut)]
+    pub user_staking_wallet : InterfaceAccount<'info , TokenAccount>,
+
+    #[account(mut)]
+    pub admin_staking_wallet : InterfaceAccount<'info , TokenAccount>,
+    
+    #[account(mut)]
+    pub staking_token : InterfaceAccount<'info , Mint>,
+    pub token_program : Interface<'info , TokenInterface>,
+    pub system_program : Program<'info , System>
+}
+
+#[derive(Accounts)]
+pub struct Unstake<'info>{
+    #[account(mut)]
+    pub user : AccountInfo<'info>,
+    #[account(mut)]
+    pub admin : AccountInfo<'info>,
+    #[account(mut)]
+    pub user_info : Account<'info , UserInfo>,
+    #[account(mut)]
+    pub user_staking_wallet : InterfaceAccount<'info , TokenAccount>,
+    #[account(mut)]
+    pub admin_staking_wallet : InterfaceAccount<'info , TokenAccount>,
+    #[account(mut)]
+    pub staking_token : InterfaceAccount<'info , Mint>,
+    pub token_program : Interface<'info , TokenInterface>,
+}
+
+#[derive(Accounts)]
+pub struct Claimreward<'info>{
+    #[account(mut)]
+    pub user : AccountInfo<'info>,
+    #[account(mut)]
+    pub admin : AccountInfo<'info>,
+    #[account(mut)]
+    pub user_info : Account<'info , UserInfo>,
+    #[account(mut)]
+    pub user_staking_wallet : InterfaceAccount<'info , TokenAccount>,
+    #[account(mut)]
+    pub admin_staking_wallet : InterfaceAccount<'info , TokenAccount>,
+    #[account(mut)]
+    pub staking_token : InterfaceAccount<'info , Mint>,
+    pub token_program : Interface<'info , TokenInterface>,
+}
+
+
+
+
+
+#[account]
+pub struct PoolInfo{
+    pub admin : Pubkey,
+    pub start_slot : u64,
+    pub end_slot : u64,
+    pub token : Pubkey
+}
+
+
+#[account]
+pub struct UserInfo{
+    pub amount : u64,
+    pub debt_reward : u64,
+    pub deposit_slot : u64
+}
+
+impl PoolInfo{
+  pub const LEN: usize = 32 + 8 + 8 + 32;
+}
+
+impl UserInfo {
+  pub const LEN: usize = 8 + 8 + 8;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
